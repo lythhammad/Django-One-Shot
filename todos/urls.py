a75de2ea5a8d467dd60fc76091e02264751adb75
lyth hammad <lythhhammad69@gmail.com>
2023-03-01 22:13:16 -0600

    Feature 8 Complete

--- todos/urls.py
+++ todos/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from todos.views import todo_list_view
+from todos.views import todo_list_view, todo_list_detail
 
 urlpatterns = [
     path("", todo_list_view, name="show_list"),
+    path("<int:id>/", todo_list_detail, name="show_detail"),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from todos.models import TodoList
 
 
@@ -6,3 +6,11 @@ def todo_list_view(request):
     todo_lists = TodoList.objects.all()
     context = {"todo_lists": todo_lists}
     return render(request, "todos/list.html", context)
+
+
+def todo_list_detail(request, id):
+    task_list = get_object_or_404(TodoList, id=id)
+    context = {
+        "task_list": task_list,
+    }
+    return render(request, "todos/detail.html", context)
